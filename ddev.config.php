adcc0236e78d493699bc83ef3d9d1f2a75709b02
Rob Loach <robloach@gmail.com>
2025-01-20 12:49:11 -0500

    Update

--- ddev.config.php
+++ ddev.config.php
@@ -6,8 +6,16 @@
  * @see https://docs.nextcloud.com/server/latest/admin_manual/configuration_server/config_sample_php_parameters.html
  */
 
-// Set the correct security policy
-header('Strict-Transport-Security: max-age=15552000; includeSubDomains');
+/**
+ * Utility function to check if we're in a CLI
+ */
+function ddev_nextcloud_cli() {
+    return (defined('STDIN')) || 
+        (php_sapi_name() === 'cli') || 
+        (array_key_exists('SHELL', $_ENV)) ||
+        (empty($_SERVER['REMOTE_ADDR']) && !isset($_SERVER['HTTP_USER_AGENT']) && count($_SERVER['argv']) > 0) ||
+        (!array_key_exists('REQUEST_METHOD', $_SERVER));
+}
 
 // Retrieve the DDEV URL
 $primary_url = getenv('DDEV_PRIMARY_URL');
@@ -15,11 +23,10 @@ $url = parse_url($primary_url);
 
 // Set the NextCloud Config
 $CONFIG = [
-    'memcache.local' => '\OC\Memcache\APCu',
     'maintenance_window_start' => 1,
     'default_phone_region' => 'US',
     'trusted_domains' => [
-        $url['host']
+        $url['host'] ?? ''
     ],
     'overwrite.cli.url' => $primary_url,
     'mail_smtpmode' => 'null',
@@ -37,5 +44,13 @@ $CONFIG = [
     'trusted_proxies' => [
         $_SERVER['SERVER_ADDR'] ?? ''
     ],
-    'overwritehost' => $url['host'],
+    'overwritehost' => $url['host'] ?? '',
 ];
+
+// Enable memcache only if we're not using the command line interface.
+if (!ddev_nextcloud_cli()) {
+    $CONFIG['memcache.local'] = '\OC\Memcache\APCu';
+
+    // Set the correct security policy
+    header('Strict-Transport-Security: max-age=15552000; includeSubDomains');
+}
